f9c0b8656174772b67510676b7dad06f065ad921
Saabithsp <saabithsp@gmail.com>
2024-06-16 10:05:45 +0530

    film app part

---
+++ app/src/main/java/com/example/movies/Activities/IntroActivity.java
@@ -0,0 +1,28 @@
+package com.example.movies.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.movies.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        Button getInBtn = findViewById(R.id.getInBtn);
+        getInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this, LoginActivity.class));
+            }
+        });
+
+    }
+}
